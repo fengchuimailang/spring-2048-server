580f8ce302a1e5e16f1916faedd44f808b0d20c4
liubo2 <liubo1@flashhold.com>
2017-10-31 19:30:46 +0800

    part of login and dependency

---
+++ src/main/java/com/liubo/game/controller/login/LoginController.java
@@ -0,0 +1,8 @@
+package com.liubo.game.controller.login;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoginController {
+	private final Logger logger = LoggerFactory.getLogger(LoginController.class);
+}
